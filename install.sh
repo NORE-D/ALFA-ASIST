311d67082a9b9cb0fb624b9cb25b3994e9b04c00
NORE <138540727+NORE-D@users.noreply.github.com>
2023-12-12 16:36:49 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt-get install python -y 
+sudo apt-get install python3 -y
+sudo apt-get install apache2 -y
+sudo apt-get install mariadb-server -y
+sudo apt-get install openssh-server -y
+sudo mv ALFA-ASIST /bin/ && chmod 777 ALFA-ASIST
+sudo mv ALFA-ASIST.py /bin/ 
